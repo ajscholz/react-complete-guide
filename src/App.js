f0899b13f218c3a80ad00bcabb924faadd222f3b
Andrew Scholz <yoitsscholzy@gmail.com>
2019-03-01 16:17:35 -0500

    completed module 5 and added CSS modules

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Person from './Person/Person';
+import appCss from './App.module.css';
 import './App.css';
 
 class App extends Component {
@@ -46,16 +47,8 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-    }
-
     let persons = null;
+    let buttonClass= '';
 
     if (this.state.showPersons) {
       persons = (
@@ -70,24 +63,20 @@ class App extends Component {
           })}
         </div>
       );
-      style.backgroundColor = 'red';
-      style[':hover'] = {
-        backgroundColor: 'salmon',
-        color: 'black',
-      }
+      buttonClass = appCss.Red;
     }
 
     // Creates dynamic styling, which is displayed inside the <p> element below
     const classes = [];
-    if (this.state.persons.length <= 2) classes.push('red');
-    if (this.state.persons.length <= 1) classes.push('bold');
+    if (this.state.persons.length <= 2) classes.push(appCss.red);
+    if (this.state.persons.length <= 1) classes.push(appCss.bold);
 
     return (
-      <div className='App'>
+      <div className={appCss.App}>
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(' ')}>This is really working!</p>
         <button
-          style={style}
+          className={buttonClass}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
       </div>
