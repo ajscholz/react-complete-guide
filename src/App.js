bc320e2336089d0b2a9fc8592263d30f4093762b
Andrew Scholz <yoitsscholzy@gmail.com>
2019-03-01 11:51:08 -0500

    completed module 4

--- src/App.js
+++ src/App.js
@@ -5,32 +5,44 @@ import Person from './Person/Person'
 class App extends Component {
   state = {
     persons: [
-      { name: 'Max', age: 28, },
-      { name: 'Manu', age: 29, },
-      { name: 'Stephanie', age: 26, },
+      { id: 'sdhjf', name: 'Max', age: 28, },
+      { id: 'weuri', name: 'Manu', age: 29, },
+      { id: 'cxvis', name: 'Stephanie', age: 26, },
     ],
     otherState: 'some other value',
+    showPersons: false,
+  };
+
+  deletePersonHandler = (personIndex) => {
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
   }
 
-  switchNameHandler = (newName) => {
-    // console.log('Was clicked!');
-    this.setState({
-      persons: [
-        { name: newName, age: 28, },
-        { name: 'Manu', age: 29, },
-        { name: 'Stephanie', age: 27, },
-      ],
-    })
+  nameChangedHandler = (event, id) => {
+    // Creates new array of people from state to maintain state immutability
+    const persons = [...this.state.persons];
+
+    // Takes given ID and finds the index of the person in state with that id
+    const personIndex = this.state.persons.findIndex(p => p.id === id)
+
+    // Creates a new JS object to maintain state immutability
+    const person = { ...this.state.persons[personIndex], }
+
+    // Now manipulate new JS Object (not original state)
+    person.name = event.target.value;
+
+    // Now update copied state array
+    persons[personIndex] = person;
+
+    // Now update state with copied arrays & objects
+    this.setState({ persons: persons })
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: 'Max', age: 28, },
-        { name: event.target.value, age: 29, },
-        { name: 'Stephanie', age: 26, },
-      ],
-    })
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    // !doesShow below simply reverses the boolean
+    this.setState({showPersons: !doesShow})
   }
 
   render() {
@@ -41,24 +53,32 @@ class App extends Component {
       padding: '8px',
       cursor: 'pointer',
     }
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person
+              click={() => this.deletePersonHandler(index)}
+              name={person.name}
+              age={person.age}
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)} />
+          })}
+        </div>
+      );
+    }
+
     return (
       <div className='App'>
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
         <button
           style={style}
-          onClick={() => this.switchNameHandler('Maximillian!!')}>Switch Name</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age} />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'Max!')}
-          changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age} />
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {persons}
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, `Hi, I'm a React Element!!!`));
